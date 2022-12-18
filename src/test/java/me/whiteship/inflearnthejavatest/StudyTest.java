d6a16195cca7cd2b59ed8501931b941c07b11a67
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-12-18 22:31:50 +0900

    JUnit5 테스트 이름 표시하기

--- src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
@@ -4,14 +4,18 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
+    @DisplayName("스터디 만들기 ╯°□°）╯")
     void create_new_study() {
         Study study = new Study();
         assertNotNull(study);
@@ -19,7 +23,7 @@ class StudyTest {
     }
 
     @Test
-    @Disabled
+    @DisplayName("스터디 만들기 \uD83D\uDE31")
     void create_new_study_again() {
         System.out.println("create1");
     }
