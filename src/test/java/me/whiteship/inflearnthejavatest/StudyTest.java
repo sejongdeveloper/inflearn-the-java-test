6c53e4cad095c00610f04c4790274766f052881d
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-12-18 22:28:27 +0900

    Initial commit

---
+++ src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
@@ -0,0 +1,46 @@
+package me.whiteship.inflearnthejavatest;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudyTest {
+
+    @Test
+    void create_new_study() {
+        Study study = new Study();
+        assertNotNull(study);
+        System.out.println("create");
+    }
+
+    @Test
+    @Disabled
+    void create_new_study_again() {
+        System.out.println("create1");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each");
+    }
+}
