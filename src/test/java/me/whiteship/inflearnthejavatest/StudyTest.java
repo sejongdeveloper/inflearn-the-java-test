8dc1de9b8ca047d725302caf25d9a13aa85c5b2e
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-12-18 22:51:37 +0900

    JUnit5 Assertion - 예외를 검증하는 방법

--- src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
@@ -19,14 +19,8 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기 ╯°□°）╯")
     void create_new_study() {
-        Study study = new Study(-10);
-
-        assertAll(
-                () -> assertNotNull(study),
-                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT + " 상태다."),
-                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
-        );
-
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        assertEquals("limit은 0보다 커야 한다.", exception.getMessage());
     }
 
     @Test
