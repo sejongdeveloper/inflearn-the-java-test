d95c574d563c2455f268a83bb365a26237c8c750
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2022-12-20 22:46:28 +0900

    Mock 객체 Stubbing - 반복 호출할 때 특정 횟수에 Stubbing 하기

--- src/test/java/me/whiteship/inflearnthejavatest/study/StudyServiceTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/study/StudyServiceTest.java
@@ -27,17 +27,21 @@ class StudyServiceTest {
         member.setId(1L);
         member.setEmail("keesun@email.com");
 
-        when(memberService.findById(any())).thenReturn(Optional.of(member));
+        when(memberService.findById(any()))
+                .thenReturn(Optional.of(member))
+                .thenThrow(new RuntimeException())
+                .thenReturn(Optional.empty())
+        ;
 
-        assertEquals("keesun@email.com", memberService.findById(1L).get().getEmail());
-        assertEquals("keesun@email.com", memberService.findById(2L).get().getEmail());
+        Optional<Member> byId = memberService.findById(1L);
+        assertEquals("keesun@email.com", byId.get().getEmail());
 
-        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
-        assertThrows(IllegalArgumentException.class, () -> {
-            memberService.validate(1L);
+        assertThrows(RuntimeException.class, () -> {
+            memberService.findById(2L);
         });
 
-        memberService.validate(2L);
+        assertEquals(Optional.empty(), memberService.findById(3L));
+
     }
 
 }
